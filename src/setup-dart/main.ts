6a8f8c817c275bf56baf210f6610220d1d625bbf
Danny Tuppeny <danny@tuppeny.com>
2019-08-27 17:18:31 +0100

    Basic implementation

---
+++ src/setup-dart/main.ts
@@ -0,0 +1,30 @@
+import * as core from "@actions/core";
+import * as tc from "@actions/tool-cache";
+import * as path from "path";
+
+const isWin = /^win/.test(process.platform);
+const isMac = process.platform === "darwin";
+
+const dartOS = isWin ? "windows" : (isMac ? "macos" : "linux");
+
+async function run() {
+	try {
+		const dartChannel = core.getInput("channel", { required: true });
+		const url = `https://storage.googleapis.com/dart-archive/channels/${dartChannel}/release/latest/sdk/dartsdk-${dartOS}-x64-release.zip`;
+
+		core.debug(`Downloading ${url}...`);
+		const dartZipPath = await tc.downloadTool(url);
+
+		// TODO: Cache?
+		// https://github.com/actions/toolkit/tree/master/packages/tool-cache
+		core.debug(`Extracting ${dartZipPath}...`);
+		const dartSdkPath = await tc.extractZip(dartZipPath);
+
+		core.addPath(path.join(dartSdkPath, "dart-sdk", "bin"));
+		core.setOutput("dart-sdk", dartSdkPath);
+	} catch (error) {
+		core.setFailed(error.message);
+	}
+}
+
+run();

6f59cc1335d461b4f708dbdfa3f7cf085ee11876
Danny Tuppeny <danny@tuppeny.com>
2019-08-27 17:56:27 +0100

    Add Flutter action

---
+++ src/setup-flutter/main.ts
@@ -0,0 +1,26 @@
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+import * as path from "path";
+import * as os from "os";
+
+async function run() {
+	try {
+		const flutterChannel = core.getInput("channel", { required: true });
+		const flutterRepo = `https://github.com/flutter/flutter`;
+
+		const tempFolder = os.tmpdir();
+		await exec.exec("git", ["clone", "--single-branch", "--branch", flutterChannel, flutterRepo], { cwd: tempFolder });
+
+		const flutterSdkPath = path.join(tempFolder, "flutter");
+
+		core.addPath(path.join(flutterSdkPath, "bin"));
+		core.addPath(path.join(flutterSdkPath, "cache", "dart-sdk", "bin"));
+		core.setOutput("flutter-sdk", flutterSdkPath);
+
+		await exec.exec("flutter", ["doctor"], { cwd: tempFolder });
+	} catch (error) {
+		core.setFailed(error.message);
+	}
+}
+
+run();

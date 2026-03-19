98275871259ddf508dba5dcc92cf844baf2ca15f
Danny Tuppeny <danny@tuppeny.com>
2026-03-19 11:09:18 +0000

    Recompile actions
    These changes are minor, just from the typescript/deps bumps and module change.

--- out/src/setup-flutter/main.js
+++ out/src/setup-flutter/main.js
@@ -1,79 +1,64 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dartOS = void 0;
-const core = require("@actions/core");
-const exec = require("@actions/exec");
-const tc = require("@actions/tool-cache");
-const fs = require("fs");
-const os = require("os");
-const path = require("path");
-const utils_1 = require("../utils");
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+import * as tc from "@actions/tool-cache";
+import * as fs from "fs";
+import * as os from "os";
+import * as path from "path";
+import { fetch } from "../utils.js";
 const flutterRepo = `https://github.com/flutter/flutter`;
 const isWin = /^win/.test(process.platform);
 const isMac = process.platform === "darwin";
 const isLinux = !(isWin || isMac);
-exports.dartOS = isWin ? "windows" : (isMac ? "macos" : "linux");
-function run() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const flutterChannel = core.getInput("channel", { required: true });
-            const useZip = core.getInput("zip") && core.getInput("zip").trim().toLowerCase() === "true";
-            let tempFolder = path.join(os.tmpdir(), Math.round(Math.random() * 10000).toString());
-            fs.mkdirSync(tempFolder);
-            // Resolve symlinks because the Dart analysis server will resolve them
-            // and sometimes give errors about types not matching across them.
-            tempFolder = fs.realpathSync.native(tempFolder);
-            const flutterSdkPath = useZip
-                ? yield downloadZip(flutterChannel, tempFolder)
-                : yield gitClone(flutterChannel, tempFolder);
-            core.addPath(path.join(flutterSdkPath, "bin"));
-            core.addPath(path.join(flutterSdkPath, "bin", "cache", "dart-sdk", "bin"));
-            core.setOutput("flutter-sdk", flutterSdkPath);
-            yield exec.exec(path.join(flutterSdkPath, "bin", isWin ? "flutter.bat" : "flutter"), ["config", "--no-analytics"]);
-            yield exec.exec(path.join(flutterSdkPath, "bin", isWin ? "flutter.bat" : "flutter"), ["doctor", "-v"]);
-        }
-        catch (error) { // eslint-disable-line @typescript-eslint/no-explicit-any
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-            const errorMessage = "message" in error ? error.message : error;
-            core.setFailed(errorMessage !== null && errorMessage !== void 0 ? errorMessage : "<unknown error>");
-        }
-    });
+export const dartOS = isWin ? "windows" : (isMac ? "macos" : "linux");
+async function run() {
+    try {
+        const flutterChannel = core.getInput("channel", { required: true });
+        const useZip = core.getInput("zip") && core.getInput("zip").trim().toLowerCase() === "true";
+        let tempFolder = path.join(os.tmpdir(), Math.round(Math.random() * 10000).toString());
+        fs.mkdirSync(tempFolder);
+        // Resolve symlinks because the Dart analysis server will resolve them
+        // and sometimes give errors about types not matching across them.
+        tempFolder = fs.realpathSync.native(tempFolder);
+        const flutterSdkPath = useZip
+            ? await downloadZip(flutterChannel, tempFolder)
+            : await gitClone(flutterChannel, tempFolder);
+        core.addPath(path.join(flutterSdkPath, "bin"));
+        core.addPath(path.join(flutterSdkPath, "bin", "cache", "dart-sdk", "bin"));
+        core.setOutput("flutter-sdk", flutterSdkPath);
+        await exec.exec(path.join(flutterSdkPath, "bin", isWin ? "flutter.bat" : "flutter"), ["config", "--no-analytics"]);
+        await exec.exec(path.join(flutterSdkPath, "bin", isWin ? "flutter.bat" : "flutter"), ["doctor", "-v"]);
+    }
+    catch (error) { // eslint-disable-line @typescript-eslint/no-explicit-any
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+        const errorMessage = "message" in error ? error.message : error;
+        const errorMessageString = typeof errorMessage === "string"
+            ? errorMessage
+            : "<unknown error>";
+        core.setFailed(errorMessageString);
+    }
 }
-function gitClone(flutterChannel, folder) {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield exec.exec("git", ["clone", "--single-branch", "--branch", flutterChannel, flutterRepo], { cwd: folder });
-        return path.join(folder, "flutter");
-    });
+async function gitClone(flutterChannel, folder) {
+    await exec.exec("git", ["clone", "--single-branch", "--branch", flutterChannel, flutterRepo], { cwd: folder });
+    return path.join(folder, "flutter");
 }
-function downloadZip(flutterChannel, folder) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const url = `https://storage.googleapis.com/flutter_infra/releases/releases_${exports.dartOS}.json`;
-        let releases;
-        try {
-            releases = JSON.parse(yield (0, utils_1.fetch)(url));
-        }
-        catch (e) {
-            throw new Error(`Failed to download Flutter releases from ${url}: ${e}`);
-        }
-        const hash = releases.current_release[flutterChannel];
-        if (!hash)
-            throw new Error(`Unable to find a release for channel ${flutterChannel}`);
-        const release = releases.releases.find((r) => r.hash === hash);
-        if (!release)
-            throw new Error(`Unable to find release for hash ${hash}`);
-        const zipPath = yield tc.downloadTool(`${releases.base_url}/${release.archive}`);
-        yield (isLinux ? tc.extractTar(zipPath, folder) : tc.extractZip(zipPath, folder));
-        return path.join(folder, "flutter");
-    });
+async function downloadZip(flutterChannel, folder) {
+    const url = `https://storage.googleapis.com/flutter_infra/releases/releases_${dartOS}.json`;
+    let releases;
+    try {
+        releases = JSON.parse(await fetch(url));
+    }
+    catch (e) {
+        throw new Error(`Failed to download Flutter releases from ${url}: ${e}`);
+    }
+    const hash = releases.current_release[flutterChannel];
+    if (!hash)
+        throw new Error(`Unable to find a release for channel ${flutterChannel}`);
+    const release = releases.releases.find((r) => r.hash === hash);
+    if (!release)
+        throw new Error(`Unable to find release for hash ${hash}`);
+    const zipPath = await tc.downloadTool(`${releases.base_url}/${release.archive}`);
+    await (isLinux ? tc.extractTar(zipPath, folder) : tc.extractZip(zipPath, folder));
+    return path.join(folder, "flutter");
 }
 void run();
 //# sourceMappingURL=main.js.map
--- out/src/utils.js
+++ out/src/utils.js
@@ -1,16 +1,12 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fetch = void 0;
-const https = require("https");
-const url = require("url");
-function fetch(urlString) {
+import * as https from "https";
+import * as url from "url";
+export function fetch(urlString) {
     const u = url.parse(urlString);
     if (u.protocol === "https:")
         return fetchHttps(u.hostname, u.port, u.path);
     else
         throw new Error(`Cannot fetch URL ${urlString}`);
 }
-exports.fetch = fetch;
 function fetchHttps(hostname, port, path) {
     return new Promise((resolve, reject) => {
         const options = {
@@ -24,11 +20,12 @@ function fetchHttps(hostname, port, path) {
         };
         const req = https.request(options, (resp) => {
             if (!resp || !resp.statusCode || resp.statusCode < 200 || resp.statusCode > 300) {
+                // eslint-disable-next-line @typescript-eslint/prefer-promise-reject-errors
                 reject({ message: `Failed to get ${path || "/"}: ${resp && resp.statusCode}: ${resp && resp.statusMessage}` });
             }
             else {
                 const chunks = [];
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-argument
                 resp.on("data", (b) => chunks.push(b.toString()));
                 resp.on("end", () => {
                     const data = chunks.join("");

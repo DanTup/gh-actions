4a33aae3164d214300b7562f88ab406b6f93f701
Danny Tuppeny <danny@tuppeny.com>
2026-03-19 11:07:38 +0000

    Update all dependencies
    Update everything (node_modules is committed separately for better history)

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+	eslint.configs.recommended,
+	...tseslint.configs.recommendedTypeChecked,
+	{
+		languageOptions: {
+			parserOptions: {
+				project: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+		rules: {
+			"@typescript-eslint/restrict-template-expressions": "off",
+		},
+	}
+);
